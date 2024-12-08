9fe15bb4f633dd4df9cbae40b160113a04096de6
Maksym Orlianskyi <87619450+OrlykM@users.noreply.github.com>
2024-12-08 21:59:57 +0200

    Update ui.py

--- NLP6/src/ui.py
+++ NLP6/src/ui.py
@@ -1,6 +1,5 @@
 import gradio as gr
 
-
 def validate_api_key(api_key):
     if api_key:
         return gr.update(interactive=True)
@@ -27,14 +26,21 @@ def create_ui(assistant_generate_answer):
             lines=1,
             interactive=True
         )
+        
+        example_queries = ["What is doi number of \"Comprehensive Performance Evaluation between Visual SLAM and LiDAR SLAM for Mobile Robots: Theories and Experiments\" ?", "How many and who are authors of article \"LIO-SAM: Tightly-coupled Lidar Inertial Odometry via Smoothing and Mapping\""]
+        example_buttons = [
+            gr.Button(example, size="sm") for example in example_queries
+        ]
+
         query_input = gr.Textbox(
             label="Enter your query",
             placeholder="What is SLAM?",
             lines=2,
             interactive=False  
         )
+        
         method_input = gr.Radio(
-            choices=['BM25', 'Full', 'Semantic'],
+            choices=['BM25', 'Semantic', 'Full'],
             label="Choose Method",
             value='BM25'
         )
@@ -42,16 +48,40 @@ def create_ui(assistant_generate_answer):
             label="Enable Reranking",
             value=True
         )
+
         context_output = gr.Textbox(label="Generated Context")
         references_output = gr.Textbox(label="References")
 
         generate_button = gr.Button("Generate Answer")
+
+        with gr.Column():
+            api_key_input
+
+        with gr.Row():
+            for button in example_buttons:
+                button
+
+        with gr.Column():
+            method_input
+            rerank_input
+            query_input
+            generate_button
+            context_output
+            references_output
+
+        for button, example in zip(example_buttons, example_queries):
+            button.click(
+                fn=lambda ex=example: ex,
+                inputs=[],
+                outputs=[query_input]
+            )
 
         api_key_input.change(
             validate_api_key,
             inputs=[api_key_input],
             outputs=[query_input]
         )
+
         generate_button.click(
             lambda api_key, query, method, rerank: generate_answer_gradio(api_key, query, method, rerank, assistant_generate_answer),
             inputs=[api_key_input, query_input, method_input, rerank_input],
